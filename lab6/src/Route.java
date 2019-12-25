17f78b4ee722230cfa50e0bd6d178783d8ea1a6d
lo0ken <dmitriy.gavrilov04@gmail.com>
2019-12-25 22:43:17 +0300

    added Route class

---
+++ lab6/src/Route.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Route {
+
+    private List<Integer> track;
+    private int distance;
+
+    public Route(Graph graph, List<Integer> track) {
+        this.track = new ArrayList<>(track);
+        calculateDistance(graph.getDistance());
+    }
+
+    private void calculateDistance(int[][] graphDistances) {
+        for (int i = 0; i < track.size() - 1; i++) {
+            distance += graphDistances[track.get(i)][track.get(i + 1)];
+        }
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
