73e013a3213a82bafb6231e0c4d0331db3b4e569
lo0ken <dmitriy.gavrilov04@gmail.com>
2019-12-26 02:24:14 +0300

    changed route class

--- lab6/src/graph/Route.java
+++ lab6/src/graph/Route.java
@@ -4,22 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Route {
-
     private List<Integer> track;
     private int distance;
+    private int[][] graphDistances;
 
     public Route(Graph graph, List<Integer> track) {
         this.track = new ArrayList<>(track);
-        calculateDistance(graph.getDistance());
+        graphDistances = graph.getDistance();
+        distance = 0;
     }
 
-    private void calculateDistance(int[][] graphDistances) {
-        for (int i = 0; i < track.size() - 1; i++) {
-            distance += graphDistances[track.get(i)][track.get(i + 1)];
-        }
+    public void addVertex(int v) {
+        track.add(v);
     }
 
     public int getDistance() {
+        distance = 0;
+        for (int i = 0; i < track.size() - 1; i++) {
+            distance += graphDistances[track.get(i)][track.get(i + 1)];
+        }
         return distance;
+    }
+
+    public List<Integer> getTrack() {
+        return track;
     }
 }
