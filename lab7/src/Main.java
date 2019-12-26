d293c5640c0aacab7261984a117f704d079681f6
lo0ken <dmitriy.gavrilov04@gmail.com>
2019-12-26 06:07:05 +0300

    added analyse time method

--- lab7/src/Main.java
+++ lab7/src/Main.java
@@ -1,13 +1,44 @@
-import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
+        analyseTime();
+    }
 
-        String s1 = Character.toString(67);
-        String s2 = Character.toString(67);
-        System.out.println(s1.equals(s2));
-        System.out.println(s1 == s2);
+    public static String generateRandomString(int length) {
+        Random random = new Random();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            builder.append(Character.toString(random.nextInt(9)));
+        }
+        return builder.toString();
+    }
 
+    public static void analyseTime() {
+        int repeats = 20;
+        try (BufferedWriter file = new BufferedWriter(new FileWriter(("Standart.txt")))) {
+            for (int i = 4; i < 1000; i+=10) {
+                long time = 0;
+                for (int j = 0; j < repeats; j++) {
+                    String s = generateRandomString(i);
+                    String sub = generateRandomString(3);
+
+                    long start = System.nanoTime();
+                    StrMatching.standard(s, sub);
+                    long end = System.nanoTime();
+
+                    time += (end - start);
+                }
+
+                file.write(i + " " + time / repeats + "\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
